3478bd9e967792251658fee6b84a3320f40445ea
Marc Jovaní González <12443324+drackp2m@users.noreply.github.com>
2026-08-03 23:15:10 +0000

    🎨 style: add crop effect to menu items, and blur as backdrop filter

--- apps/web/src/app/layout/main/component/nav.component.ts
+++ apps/web/src/app/layout/main/component/nav.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 
-import { ButtonDirective } from '@app/directive/button.directive';
 import { RouterLinkDirective } from '@app/directive/router-link.directive';
 
 @Component({
 	selector: 'app-nav',
 	templateUrl: './nav.component.html',
 	styleUrl: './nav.component.scss',
-	imports: [RouterLinkDirective, ButtonDirective],
+	imports: [RouterLinkDirective],
 })
 export class NavComponent {}
