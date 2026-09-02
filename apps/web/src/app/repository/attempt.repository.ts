18906b1dd66e5cfdbf8a0c9029c993d609dd2ddb
Marc Jovaní González <12443324+drackp2m@users.noreply.github.com>
2026-09-02 03:30:35 +0000

    ✨ feat: query local attempts by completion date

--- apps/web/src/app/repository/attempt.repository.ts
+++ apps/web/src/app/repository/attempt.repository.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
 
 import { AppSchema } from '@app/repository/definition/app-schema.interface';
+import { AttemptRow } from '@app/repository/definition/attempt-schema.interface';
 import { GenericRepository } from '@app/repository/generic.repository';
 
 @Injectable({
 	providedIn: 'root',
 })
-export class AttemptRepository extends GenericRepository<AppSchema> {}
+export class AttemptRepository extends GenericRepository<AppSchema> {
+	findRangeByUpdatedAt(from: Date, to: Date): Promise<AttemptRow[]> {
+		return this.findAllByIndex('attempt', 'updatedAt', IDBKeyRange.bound(from, to));
+	}
+}
